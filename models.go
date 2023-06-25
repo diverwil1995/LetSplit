8f1f3ba16b522d01731d563fc6d55fa61b60faa1
smdick1995 <smdick1995@gmail.com>
2023-06-26 00:58:12 +0800

    refactor: add definition of repo in repository.go, Add validation for User struct in models.go

--- models.go
+++ models.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"crypto/sha256"
+	"encoding"
+	"errors"
+	"time"
+	"unicode"
+)
 
 type User struct {
 	Id       int    `json:"id"`
@@ -8,6 +14,81 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"-"`
 }
+
+var (
+	ErrUsernameEmpty = errors.New("username is empty")
+	ErrPasswordEmpty = errors.New("password is empty")
+)
+
+func (u *User) Validate() error {
+	if u == nil {
+		return nil
+	}
+	if u.Username == "" {
+		return ErrUsernameEmpty
+	}
+	if u.Password == "" {
+		return ErrPasswordEmpty
+	}
+	return nil
+}
+func (u *User) CheckPassword() (bool, error) {
+	const (
+		minLenth = 10
+		maxLenth = 25
+	)
+	var (
+		ErrTooShort = errors.New("password is too short")
+		ErrTooLong  = errors.New("password is too long")
+		hasSpecial  = false
+		hasUpper    = false
+		hasLower    = false
+		hasDigit    = false
+	)
+	if len(u.Password) < minLenth {
+		return false, ErrTooShort
+	}
+	if len(u.Password) > maxLenth {
+		return false, ErrTooLong
+	}
+	for _, v := range u.Password {
+		if unicode.IsSymbol(v) {
+			hasSpecial = true
+		}
+		if unicode.IsUpper(v) {
+			hasUpper = true
+		}
+		if unicode.IsLower(v) {
+			hasLower = true
+		}
+		if unicode.IsNumber(v) {
+			hasDigit = true
+		}
+		if hasSpecial && hasUpper && hasLower && hasDigit {
+			break
+		}
+	}
+	return true, nil
+}
+
+var (
+	ErrPasswordHashFailed = errors.New("password hash failed")
+)
+
+func (u User) HashPassword() ([]byte, error) {
+	hash := sha256.New()
+	hash.Write([]byte(u.Password))
+	marshaler, ok := hash.(encoding.BinaryMarshaler)
+	if !ok {
+		return nil, ErrPasswordHashFailed
+	}
+	pwd, err := marshaler.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	return pwd, nil
+}
+
 type Group struct {
 	Id     int    `json:"id"`
 	Uuid   string `json:"uuid"`
--- repository.go
+++ repository.go
@@ -2,9 +2,25 @@ package main
 
 type GetGroupsFilter struct {
 }
+type GetMembersFilter struct {
+}
+type GetExpensesFilter struct {
+}
 
 type LetSplitRepository interface {
+	AuthUser(*User) (bool, error)
+	CreateUser(*User) error
+	DeleteUser(*User) error
 	CreateGroup(*Group) error
 	GetGroups(*GetGroupsFilter) ([]Group, error)
+	UpdateGroup(*Group) error
 	DeleteGroup(*Group) error
+	AddMember(*Member) error
+	GetMembers(*GetMembersFilter) ([]Member, error)
+	UpdateMember(*Member) error
+	DeleteMember(*Member) error
+	AddExpense(*Expense) error
+	GetExpenses(*GetExpensesFilter) ([]Expense, error)
+	UpdateExpense(*Expense) error
+	DeleteExpense(*Expense) error
 }
