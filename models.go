eb8902b74e5644ec8840ba72e2d29947f637736b
smdick1995 <smdick1995@gmail.com>
2023-06-26 00:22:10 +0800

    refactor: add definition of data in models.go

--- models.go
+++ models.go
@@ -2,36 +2,31 @@ package main
 
 import "time"
 
+type User struct {
+	Id       int    `json:"id"`
+	Uuid     string `json:"uuid"`
+	Username string `json:"username"`
+	Password string `json:"-"`
+}
 type Group struct {
-	Id      int
-	Uuid    string
-	Name    string
-	Members []*Member
-	Records []*Record
-	Report  *Report
+	Id     int    `json:"id"`
+	Uuid   string `json:"uuid"`
+	UserId int    `json:"user_id"`
+	Title  string `json:"title"`
 }
 type Member struct {
-	Id   int
-	Uuid string
-	Name string
+	Id      int     `json:"id"`
+	Uuid    string  `json:"uuid"`
+	GroupId int     `json:"group_id"`
+	Name    string  `json:"name"`
+	Debt    float64 `json:"debt"`
 }
 type Expense struct {
-	Id     int
-	Uuid   string
-	Date   time.Time
-	What   string
-	Amount float64
-	By     *Member
-	For    []*Member
-	Note   string
-}
-type Record struct {
-	Id       int
-	Uuid     string
-	Expenses []*Expense
-	DebtList []*Debt
-}
-type Debt struct {
-}
-type Report struct {
+	Id             int       `json:"id"`
+	Uuid           string    `json:"uuid"`
+	What           string    `json:"what"`
+	Date           time.Time `json:"date"`
+	Cost           float64   `json:"cost"`
+	PayerId        int       `json:"payer_id"`
+	ParticipantsId []int     `json:"participants_id"`
 }
